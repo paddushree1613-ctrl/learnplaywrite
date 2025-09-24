0af27dc20529177d717c34db486b053e7f40413f
paddushree1613-ctrl <paddushree1613@gmail.com>
2025-09-24 19:43:42 +0530

    Update Addemp.Datadriven.spec.js

--- tests/Addemp.Datadriven.spec.js
+++ tests/Addemp.Datadriven.spec.js
@@ -1,1 +1,54 @@
+import { test , expect } from '@playwright/test';
+
+const credentials = {
+username: "Admin",
+password: "admin123"
+
+};
+
+const addemployees = {
+    emp1:{
+    empfullname : "paddu",
+    middlename: 'shree',
+    lastname: 'm',
+    },
+    
+    emp2:{
+        empfullname:"rakshitha",
+        middlename: "m",
+        lastname: "shree",
+    },
+    emp3:{
+    empfullname:"anusha",
+    middlename:'anu',
+    lastname:'m'
+}
+}
+   
+
+for (let EMPADD in addemployees) {
+test (`Add employee Delails using datadriven -${addemployees[EMPADD].empfullname}`, async ({page}) => {
+    //Login
+    await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/pim/addEmployee")    
+    await page.locator("//input[@placeholder='Username").fill(credentials.username);
+    await page.locator("//input[@placeholder='Password']").fill(credentials.passaword);
+    await page.locator("//button[contains(.,'Login')]").click();
+        await page.locator("//input[@placeholder='First Name']").fill[EMPAdd].firstname);
+        await page.locator("//input[@placeholder='Middle Name']").fill[EMPAdd].lastname);
+        await page.locator("//input[@placeholder='Last Name']").fill[EMPAdd].middlename);
+        await page.locator("//input[@fdprocessedid='7q1wq']").fill
+)}
+
+
+        
+    
+
+
+
+
+
+
+
+
+
 
